565bbe4be87096e7b04623911bf3a8c10dfd5eed
llow4 <llow4@calstatela.edu>
2026-03-28 12:01:58 -0700

    Fixed multi-word queries

--- cli.js
+++ cli.js
@@ -2,9 +2,9 @@
 The CLI is the main entry point for the application. This file handles all user interaction and
 connects with app.js to perform the main fucntions of the app.
 
-node cli.js --help -> Displays all available commands/parameters
-node cli.js search -> Providing a specific keyword returns a specific movie result or a list of related results
-node cli.js history keywords -> Provides a list of all searched keywords, and prompts the user to select one to view the related movie results or exit
+node cli.js --help -> Displays all available commands/parameters 
+node cli.js search -> Providing a specific keyword returns a specific movie result or a list of related results, prompting the user to select one to view.
+ser to select onnode cli.js history keywords -> Provides a list of all searched keywords.
 */
 
 // Imports
@@ -12,43 +12,30 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { runSearchFlow, runHistoryFlow } from './app.js';
 
-// Printer Function for help commands
-function printHelp() {
-    console.log('Usage: node cli.js <command> [options]');
-    console.log('Commands:');
-    console.log('  search <keyword>   Search for movies by <keyword>');
-    console.log('  history <keywords> Display list of all searched keywords, and allows selection of a previously searched keyword');
-    console.log('  --help             Display this help message');
-}
-
 // Configure yargs to handle command line arguments and commands
 yargs(hideBin(process.argv))
     .usage('Usage: $0 <command> [options]')
-    /*
-    .command( // help command
-        '--help',
-        'Displays all available commands/parameters',
-        () => {}, // no arguments
-        () => { printHelp(); } // handler
-    )
-        */
     .command( // history command
         'history keywords',
-        'Display search history and asks user to select a searched keyword to view related movie results or exit',
+        'Display search history from previous searches.',
         () => {}, // no arguments
         () => { runHistoryFlow(); } // handler
     )
     .command( // search command
-        'search <keyword>',
-        'Search for movies by <keyword>',
+        'search <keywords>',
+        'Search for movies by <keywords>',
         (yargs) => {
             yargs
-                .positional('keyword', {
+                .positional('keywords', {
                     describe: 'Keyword to search for movies',
                     type: 'string'
                 });
         },
-        (argv) => { runSearchFlow(argv.keyword); } // handler
+        (argv) => { // handler
+            // Concatenates keywords if there are multiple
+            const joinedKeywords = Array.isArray(argv.keywords) ? argv.keywords.join(' ') : String(argv.keywords); 
+            runSearchFlow(joinedKeywords); 
+        } 
     )
     .help()
     .parse();
