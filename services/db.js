4cf038938659587113e3117ff5508c432939cc48
Joe Miranda <jmiran125@calstatela.edu>
2026-05-05 13:05:53 -0700

    Create dependencies and db.js

--- services/db.js
+++ services/db.js
@@ -8,4 +8,43 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Create MongoDB connection string using env variables
-const uri = 'mongodb+srv://$'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+// Store database connection
+let db;
+
+// Funch to connect to MongoDB Atlas
+async function connectToDB() {
+    try {
+        // If the database connection already exists, return it
+        if (db) {
+            return db;
+        }
+
+        // Connect to MongoDB Atlas
+        await client.connect();        
+
+        // Select the database
+        db = client.db(process.env.DB_NAME);
+        console.log('Connected to MongoDB Atlas');
+
+        return db;
+    }
+    catch (error) {
+        console.log('Error connecting to MongoDB Atlas:', error.message);
+        throw error;
+    }
+}
+
+// Function to get database connection
+async function getDB() {
+    if (!db) {
+        throw new Error('Database connection not established. Call connectToDB() first.');        
+    }
+    return db;
+}
+
+// Export the functions
+export {connectToDB, getDB};
